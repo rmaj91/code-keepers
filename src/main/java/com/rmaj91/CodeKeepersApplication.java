a289681aa7ca294fd87d3e5be9a24265a25808e3
Rafal Maj <rmaj91@gmail.com>
2020-10-31 09:40:16 +0100

    init

---
+++ src/main/java/com/rmaj91/CodeKeepersApplication.java
@@ -0,0 +1,13 @@
+package com.rmaj91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeKeepersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeKeepersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmaj91/CodeKeepersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmaj91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeKeepersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
